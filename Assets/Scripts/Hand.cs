67c1f7b62dbd93502cff9708d2cdabd2dce11b8a
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2022-11-29 14:38:10 +0900

    Hand

--- Assets/Scripts/Hand.cs
+++ Assets/Scripts/Hand.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class Hand : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public string handName;     //너클이나 맨손 구분
+    public float range;         //공격범위
+    public int damage;          //공격력
+    public float workSpeed;     //작업 속도
+    public float attackDelay;   //공격딜레이
+    public float attackDelayA;   //공격 활성화 시점
+    public float attackDelayB;   //공격 비활성화 시점
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Animator anim;       //애니메이션
+
 }
