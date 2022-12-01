47e8d760d3b63b733ee9ceec86f7304447ba7e7f
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2022-12-01 19:55:30 +0900

    Gun & Gun Anim

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public string gunName;  //총의 이름
+    public float range;     //사정 거리
+    public float accuracy;  //정확도
+    public float fireRate;  //연사 속도
+    public float reloadTime;//재장전 속도
+
+    public int damage;      //총의 데미지    
+
+    public int reloadBulletCount;   //총의 재장전 개수
+    public int currentBulletCount;  //현재 탄알집에 남아있는 총알의 갯수
+    public int maxBulletCount;      //최대 소유 가능 총알 갯수
+    public int carryBulletCount;    //현재 소유하고 있는 총알 갯수
+
+    public float retroActionForce;  //반동 세기
+    public float retroActionFineSightForce; //정조준시의 반동 세기
+
+    public Vector3 fineSightOriginPos;      //정조준시 정해진 위치
+    public Animator anim;
+    public ParticleSystem muzzleFlesh;
+
+    public AudioClip fire_Sound;
+
 }
