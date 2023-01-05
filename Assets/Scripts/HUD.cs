9eb58e7ac3028013d4ce0f54a6f23a582ae29cf0
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2023-01-05 22:06:57 +0900

    HUD (총알)

--- Assets/Scripts/HUD.cs
+++ Assets/Scripts/HUD.cs
@@ -1,20 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class HUD : MonoBehaviour
 {
-    [SerializeField] private 
+    // 필요한 컴포넌트 
+    [SerializeField] private GunController theGunController;
+    private Gun currentGun;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // 필요하면 HUD 호출, 필요 없으면 HUD 비활성화 
+    [SerializeField] private GameObject go_BulletHUD;
+
+    // 총알 갯수 텍스트에 반영
+    [SerializeField] private TMP_Text[] text_Bullet;
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        CheckBullet();
+    }
+
+    private void CheckBullet()
+    {
+        currentGun = theGunController.GetGun();
+        text_Bullet[0].text = currentGun.carryBulletCount.ToString();
+        text_Bullet[1].text = currentGun.reloadBulletCount.ToString();
+        text_Bullet[2].text = currentGun.currentBulletCount.ToString();
     }
 }
