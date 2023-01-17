d47432a00b27fce8eb3755e37d96a1933001294f
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2023-01-17 13:46:00 +0900

    weapon set

--- Assets/Scripts/WeaponManager.cs
+++ Assets/Scripts/WeaponManager.cs
@@ -24,13 +24,13 @@ public class WeaponManager : MonoBehaviour
     [SerializeField] private Hand[] hands;
 
     // 관리 차원에서 쉽게 무기 접근이 가능하도록 만듦
-    private Dictionary<string, Gun> gunDictionary = new Dictionary<string, Gun>();   
+    private Dictionary<string, Gun> gunDictionary = new Dictionary<string, Gun>();
     private Dictionary<string, Hand> handDictionary = new Dictionary<string, Hand>();
 
     // 필요한 컴포넌트
     [SerializeField] private GunController theGunController;
-    [SerializeField] private HandController theHandController; 
-    
+    [SerializeField] private HandController theHandController;
+
 
 
     // Start is called before the first frame update
@@ -39,15 +39,24 @@ public class WeaponManager : MonoBehaviour
 
         for (int i = 0; i < guns.Length; i++)
         {
-            gunDictionary.Add(guns[i].gunName,guns[i]);
+            gunDictionary.Add(guns[i].gunName, guns[i]);
 
         }
-
+        for (int i = 0; i < hands.Length; i++)
+        {
+            handDictionary.Add(hands[i].handName, hands[i]);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!isChangeWeapon)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+
+            }
+        }
     }
 }
